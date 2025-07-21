a57b72ba3c787dd411eed1a3e68b7b6314537afd
Joaovictor100 <joaovictorsts100@gmail.com>
2025-07-21 13:16:57 -0300

    Melhorando função print

--- src/asm-arm/output.s
+++ src/asm-arm/output.s
@@ -3,7 +3,6 @@ print:
     stp x29, x30, [sp, #-16]!
     mov x29, sp
     mov x3, x0
-    mov x1, x0
     bl len_string
 
     mov x2, x0
@@ -18,11 +17,12 @@ print:
 len_string:
     stp x29, x30, [sp, #-16]!
     mov x29, sp
+    mov x11, x0
     mov x12, #0
 
 loop_len_string:
-    ldrb w11, [x1], #1
-    cmp w11, #0
+    ldrb w13, [x11], #1
+    cmp w13, #0
     beq end_len_string
     add x12, x12, #1
     b loop_len_string
